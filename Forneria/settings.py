96d2217d0ec4a006e4853c3f7efb82555dc00bb5
Frankk24-coder <franco.ramirez24@inacapmail.cl>
2025-11-05 00:00:03 -0300

    Creacion de nuevos templates y estylos

--- Forneria/settings.py
+++ Forneria/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 LOGIN_URL = 'login'
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -143,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Forneria/urls.py
+++ Forneria/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
+# importamos vista desde el template/
+from ventas import views as ventas_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ventas_views.home, name='home'),
+    path('login/', ventas_views.login_view, name='login'),
+    path('registro/', ventas_views.register_view, name='registro'),
+    path('dashboard/', ventas_views.dashboard_view, name='dashboard'),
 ]
